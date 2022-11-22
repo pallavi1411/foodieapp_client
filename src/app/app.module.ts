86e93777709429e09cf433f2f5d36df27be6285e
kadama017@gmail.com <kadama017@gmail.com>
2022-11-22 17:08:21 +0530

    dn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ItemComponent } from './item/item.component';
+import { FoodlistComponent } from './foodlist/foodlist.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    RestaurantComponent,
+    ItemComponent,
+    FoodlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
